001e39550f0c8f3abb05acfed87189f32deeb1b8
Mohammad Anoshehpour <anooshehpour@hotmail.co.uk>
2012-12-04 23:04:11 +0000

    Updated DataStorage
    Reflecting changes needed for Manager

--- code/src/DataStorage.java
+++ code/src/DataStorage.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 
 public class DataStorage {
 
-	
-	//  lists of all maintainable data
-	ArrayList<Client> clientList;
-	ArrayList<Manager> managerList;
-	ArrayList<Engineer> engineerList;
-	ArrayList<Product> productList;
-	ArrayList<bugModel> bugList;
+	// lists of all maintainable data
+	private static ArrayList<Client> clientList;
+	private static ArrayList<Manager> managerList;
+	private static ArrayList<Engineer> engineerList;
+	private static ArrayList<Product> productList;
+	private static ArrayList<bugModel> bugList;
+
+	public DataStorage() {
+		clientList = new ArrayList<Client>();
+		managerList = new ArrayList<Manager>();
+		engineerList = new ArrayList<Engineer>();
+		productList = new ArrayList<Product>();
+		bugList = new ArrayList<bugModel>();
+	}
 
 	public void addClient(final Client aClient) {
 		clientList.add(aClient);
@@ -29,8 +36,8 @@ public class DataStorage {
 	public void addBug(final bugModel aBug) {
 		bugList.add(aBug);
 	}
-	
-	//Remove methods 
+
+	// Remove methods
 	public void removeClient(final Client aClient) {
 		clientList.remove(aClient);
 	}
@@ -51,25 +58,43 @@ public class DataStorage {
 		bugList.add(aBug);
 	}
 
-	
-	//Getters to access list objects 
-	public ArrayList<Client> getClientList() {
+	public static void setClientList(ArrayList<Client> clientList) {
+		DataStorage.clientList = clientList;
+	}
+
+	public static void setManagerList(ArrayList<Manager> managerList) {
+		DataStorage.managerList = managerList;
+	}
+
+	public static void setEngineerList(ArrayList<Engineer> engineerList) {
+		DataStorage.engineerList = engineerList;
+	}
+
+	public static void setProductList(ArrayList<Product> productList) {
+		DataStorage.productList = productList;
+	}
+
+	public static void setBugList(ArrayList<bugModel> bugList) {
+		DataStorage.bugList = bugList;
+	}
+
+	public static ArrayList<Client> getClientList() {
 		return clientList;
 	}
 
-	public ArrayList<Manager> getManagerList() {
+	public static ArrayList<Manager> getManagerList() {
 		return managerList;
 	}
 
-	public ArrayList<Engineer> getEngineerList() {
+	public static ArrayList<Engineer> getEngineerList() {
 		return engineerList;
 	}
 
-	public ArrayList<Product> getProductList() {
+	public static ArrayList<Product> getProductList() {
 		return productList;
 	}
 
-	public ArrayList<bugModel> getBugList() {
+	public static ArrayList<bugModel> getBugList() {
 		return bugList;
 	}
 
